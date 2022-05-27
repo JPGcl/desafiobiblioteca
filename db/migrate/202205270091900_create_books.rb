425ea3e985403f52dbbfa6167045a262f0650131
JPGcl <JPGutierrezOrtega@gmail.com>
2022-05-27 09:28:13 -0400

    Creación Modelo y migración.

---
+++ db/migrate/202205270091900_create_books.rb
@@ -0,0 +1,14 @@
+class CreateBooks < ActiveRecord::Migration[7.0]
+    def change
+      create_table :books do |t|
+        t.string    :title
+        t.string    :author
+        t.string    :status
+        t.datetime  :loan_date
+        t.datetime  :return_date
+  
+        t.timestamps
+      end
+    end
+  end
+  
